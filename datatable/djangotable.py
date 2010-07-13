c4890987e294bf31d3d5c26e4d4b856ecefa6ec7
George Vilches <gav@thataddress.com>
2010-07-13 16:54:01 -0400

    First crack at prepare_columns being smart like Django admin.

--- datatable/djangotable.py
+++ datatable/djangotable.py
@@ -3,6 +3,7 @@ from plugins import *
 from ..general import get_default_fields
 from django.db.models import Count
 from django.db.models.fields import FieldDoesNotExist
+from django.forms.forms import pretty_name
 
 
 class ModelTable(BaseTable):
@@ -11,16 +12,26 @@ class ModelTable(BaseTable):
             # When columns are specified, only use them as specified.
             new_columns = []
             for column in columns:
-                if isinstance(column, basestring):
+                if isinstance(column, (list, tuple)):
+                    new_columns.append(column)
+                else:
                     # Get labels for what we can.
                     if queryset.model:
                         try:
                             f = queryset.model._meta.get_field(column)
-                            new_columns.append([column, f.verbose_name.title()])
+                            new_columns.append([column, f.verbose_name])
                         except FieldDoesNotExist:
-                            new_columns.append([column, column])
-                else:
-                    new_columns.append(column)
+                            if callable(column):
+                                column_verbose = getattr(column, 'short_description', pretty_name(column.func_name))
+                            else:
+                                attr = getattr(queryset.model, column, None)
+                                if attr is not None and callable(attr):
+                                        column_verbose = getattr(attr, 'short_description', pretty_name(attr.func_name))
+                                else:
+                                    column_verbose = pretty_name(column)
+                            new_columns.append([column, column_verbose])
+                    else:
+                        new_columns.append([column, pretty_name(column)])
             return new_columns
         else:
             # Columns not known, pull them from the model.
@@ -28,11 +39,10 @@ class ModelTable(BaseTable):
                 return get_default_fields(queryset.model, (), exclude or None, include_verbose=True)
     
     def get_data(self, row_data, column_name):
-        opts = row_data._meta
         try:
-            f = opts.get_field(column_name)
+            f = row_data._meta.get_field(column_name)
         except FieldDoesNotExist:
-            # For non-field values, the value is either a method, property or
+            # For non-field values, the value is either a method or
             # returned via a callable.
             if callable(column_name):
                 attr = column_name
