696f9d59dac255c9d4965b7d3da98647f7064fe5
Trey Long <trey@ktrl.com>
2011-05-13 16:36:16 -0400

    rewrote binary fields to hopefully not require a backend.

--- fields/binary.py
+++ fields/binary.py
@@ -1,5 +1,17 @@
+"""
+Caution, introspection will not work unless a custom backend is also provided.
+"""
 from django.db.models.fields import Field
+from django.db.models import SubfieldBase
 from django.forms import fields as form_fields
+
+try:
+    import cStringIO
+    from cStringIO import StringIO
+    S_TYPES = (cStringIO.InputType, cStringIO.OutputType)
+except ImportError:
+    from StringIO import StringIO
+    S_TYPES = (StringIO,)
 
 #Add south migration rules.
 try:
@@ -11,37 +23,51 @@ try:
 except ImportError:
     pass
 
+class BinaryFieldBase(Field):
+    __metaclass__ = SubfieldBase
+    def to_python(self, value):
+        if isinstance(value, S_TYPES):
+            return value
+        if value is None:
+            return StringIO()
+        return StringIO(value)
 
-class BinaryField(Field):
+    def get_prep_value(self, value):
+        if hasattr(value, 'read'):
+            value = value.read()
+        if len(value) == 0:
+            value = None
+        print 'DB PREP2', type(value)
+        return value
+
+    def get_prep_lookup(self, lookup_type, value):
+        raise TypeError('Binary Fields do not support %s' % lookup_type)
+    
+class BinaryField(BinaryFieldBase):
     """Sometimes we have fields that need to store a small amount of binary
     data. This is different then a varchar on postgresql at least because it
     will not allow fields that are just null bytes.
     """
+    def __init__(self, max_length, *args, **kwargs):
+        self.max_length = max_length
+        super(BinaryField, self).__init__(*args, **kwargs)
 
-    def get_internal_type(self):
-        return 'BinaryField'
-
-    def get_prep_value(self, value):
-        if type(value) == buffer:
-            return value
-        if hasattr(value, 'read'):
-            return buffer(value.read())
-        return None
+    def db_type(self, connection):
+        if connection.settings_dict['ENGINE'] == 'django.db.backends.mysql':
+            return 'varbinary(%(max_length)s)' % self.max_length
+        else:
+            raise Exception('Mysql is the only defined engine for BinaryField.')
 
 
-class BlobField(Field):
+class BlobField(BinaryFieldBase):
     """Sometimes we have fields that need to store a large amounts of binary
     data. 
     """
-    def get_internal_type(self):
-        return 'BlobField'
-
-    def get_prep_value(self, value):
-        if type(value) == buffer:
-            return value
-        if hasattr(value, 'read'):
-            return buffer(value.read())
-        return None
+    def db_type(self, connection):
+        if connection.settings_dict['ENGINE'] == 'django.db.backends.mysql':
+            return 'longblob'
+        else:
+            raise Exception('Mysql is the only defined engine for BlobField.')
 
 
 class InlineImageField(BlobField):
